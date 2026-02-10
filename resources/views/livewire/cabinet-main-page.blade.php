cf56473428a1a2a4668b693f37fd266f73cc9608
unknown <8540462@gmail.com>
2026-02-10 13:12:32 +0200

    check cabinet main

--- resources/views/livewire/cabinet-main-page.blade.php
+++ resources/views/livewire/cabinet-main-page.blade.php
@@ -1,23 +1,23 @@
 <div class="overflow-hidden relative h-[100px] bg-blue-600 w-full flex items-center">
-    @if(!empty($rates))
-        <div id="marquee-wrapper" class="w-full overflow-hidden">
-            <div id="marquee" class="flex space-x-12">
-                @foreach($rates as $coin => $price)
-                    <div class="flex-shrink-0 px-6">
-                        <span class="font-bold text-white text-xl">{{ strtoupper($coin) }}: </span>
-                        <span class="font-bold text-white text-xl show-rate">${{ number_format($price, 2) }} </span>
-                    </div>
-                @endforeach
-                <!-- Дублируем элементы, чтобы анимация была плавной и бесконечной -->
-                @foreach($rates as $coin => $price)
-                    <div class="flex-shrink-0 px-6">
-                        <span class="font-bold text-white text-xl">{{ strtoupper($coin) }} :</span>
-                        <span class="font-bold text-white text-xl show-rate">${{ number_format($price, 2) }}</span>
-                    </div>
-                @endforeach
-            </div>
-        </div>
-    @endif
+{{--    @if(!empty($rates))--}}
+{{--        <div id="marquee-wrapper" class="w-full overflow-hidden">--}}
+{{--            <div id="marquee" class="flex space-x-12">--}}
+{{--                @foreach($rates as $coin => $price)--}}
+{{--                    <div class="flex-shrink-0 px-6">--}}
+{{--                        <span class="font-bold text-white text-xl">{{ strtoupper($coin) }}: </span>--}}
+{{--                        <span class="font-bold text-white text-xl show-rate">${{ number_format($price, 2) }} </span>--}}
+{{--                    </div>--}}
+{{--                @endforeach--}}
+{{--                <!-- Дублируем элементы, чтобы анимация была плавной и бесконечной -->--}}
+{{--                @foreach($rates as $coin => $price)--}}
+{{--                    <div class="flex-shrink-0 px-6">--}}
+{{--                        <span class="font-bold text-white text-xl">{{ strtoupper($coin) }} :</span>--}}
+{{--                        <span class="font-bold text-white text-xl show-rate">${{ number_format($price, 2) }}</span>--}}
+{{--                    </div>--}}
+{{--                @endforeach--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    @endif--}}
 
     <style>
         #marquee-wrapper {
