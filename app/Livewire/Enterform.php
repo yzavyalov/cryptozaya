e8f81f2ffa3e22823898c1d5db261d8438120314
unknown <8540462@gmail.com>
2026-02-24 15:26:20 +0200

    fix codeSent in Enterform

--- app/Livewire/Enterform.php
+++ app/Livewire/Enterform.php
@@ -7,7 +7,7 @@ use Livewire\Component;
 
 class Enterform extends Component
 {
-    public bool $codeSent = false;
+    public $codeSent = false;
     public string $email = '';
     public string $code = '';
     public int $codeTtl = 600; // 10 минут
